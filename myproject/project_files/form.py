2b039a3a3e6dc130e082ac396968e4c459bd06e6
Daniel <sisilickmyass@gmail.com>
2022-09-03 22:27:54 +0200

    cosmetics changes

--- myproject/project_files/form.py
+++ myproject/project_files/form.py
@@ -5,4 +5,4 @@ from wtforms.validators import DataRequired, Length
 
 class City(FlaskForm):
   name = StringField('City name', validators=[DataRequired(), Length(max=30)])
-  submit = SubmitField('Send')
+  submit = SubmitField('Check')
